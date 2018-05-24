ceaab90132d879798ffa33f43de85deaab6829de
gillbalraj <balrajgill18@gmail.com>
2018-05-24 18:08:05 -0400

    add /todos and /todosid

--- server.js
+++ server.js
@@ -1,13 +1,51 @@
 var express = require('express');
-
 var app = express();
+var PORT = process.env.PORT || 3000;
 
-var port = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: 'call to ask for tire-change & balancing',
+	completed: false
+}, {
+	id: 2,
+	description: 'book an appointment for tire change',
+	completed: false
+}, {
+	id: 3,
+	description: 'decide your dinner menu',
+	completed: true
+}];
 
 app.get('/', function(req, res){
-	res.send('ToDO API Root');
+	res.send('To-DO API Root hits');
 });
 
-app.listen(port, function(){
-	console.log('express is running at port: '+ port)
+// get todos
+
+app.get('/todos', function(req, res){
+	res.json(todos);
+})
+
+//get todos by id
+
+app.get('/todos/:id', function(req, res){
+
+	var todoId = parseInt(req.params.id,10);
+	var matchedTodo;
+
+	todos.forEach(function(todo){
+		if(todoId === todo.id){
+			matchedTodo = todo;
+			}
+		}) ;
+
+		if(matchedTodo){
+			res.json(matchedTodo);
+		}else{
+			res.status(404).send();
+		}
+});
+
+app.listen(PORT, function(){
+	console.log('express is running at port: '+ PORT)
 })
