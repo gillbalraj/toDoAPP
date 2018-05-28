1ca4281ca9c45312915c9cad4f5880b103bf7be7
gillbalraj <balrajgill18@gmail.com>
2018-05-28 14:49:50 -0400

    Add PUT todos

--- server.js
+++ server.js
@@ -63,6 +63,37 @@ app.delete('/todos/:id', function(req, res){
 		res.json(matchedTodo);
 	}
 })
+//PUT /todos/:id
+app.put('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if(!matchedTodo){
+		return res.status(404).send();
+	}
+	//validations
+	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttributes.completed = body.completed;
+	} else if(body.hasOwnProperty('completed')){
+			//bad
+			return res.status(400).send();
+	} else{
+		//never provided the attributed, no problem
+	}
+
+	//validate the description
+	if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+		validAttributes.description = body.description;
+	} else if(body.hasOwnProperty('description')){
+			//bad
+			return res.status(400).send();
+	}
+	//update data
+	 _.extend(matchedTodo, validAttributes);
+	 res.json(matchedTodo);
+});
 
 app.listen(PORT, function(){
 	console.log('express is running at port: '+ PORT)
